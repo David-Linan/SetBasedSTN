f3e8219e94af3b88154ad3b52acbd62a2284e615
David-Linan <dada2828@gmail.com>
2025-09-10 13:19:57 +0200

    Updating and automating plot function

--- hexaly_benchamerk_plots.py
+++ hexaly_benchamerk_plots.py
@@ -3,92 +3,245 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
+def is_real_numeric(value):
+    return np.issubdtype(type(value), np.number) and (not np.iscomplexobj(value)) and (np.isfinite(value))
 
-def plot_data_profiles(file_path, success_criterion="gap",tau=0.01,upper=1.0):
-    """
-    The x-axis represents a dimensionless quality.
-    The y-axis represents the fraction of problems successfully solved for a given quality.
-    """
-    plt.figure(figsize=(10, 6))  
-    tau_range = np.arange(0,upper,tau)
+def performance_plots(# file info
+                        file_paths: list[str],
+                        sheet_names: list[str],
+                       alg_names: list[str],
+                       best_obj_header: str,
+                       time_header: str,
+                       bound_header: str,
+                       gap_header: str,
+                       # definition of success is assumed to be in the file
+                       success_header: str,
+                       success_input: list,    
 
-    if success_criterion == "gap":
-        column_name='gap quality'
-        x_name='Optimality gap'
-    elif success_criterion == "objective":
-        column_name='Obj quality'
-        x_name='Dimensionless distance between the objective and the best known objective'
-    elif success_criterion == "bound":
-        column_name='Best bound quality'
-        x_name='Dimensionless distance between the lower bound and the best known lower bound'
-    elif success_criterion == "time":
-        column_name='time quality'
-        x_name='Dimensionless time'
+                        # performance plot to generate
+                       success_criterion: str="objective",
+                       tau: float=0.01,
+                       dimensionless_x_axis=False
+                       ):
+
+
+    # 1. Read files
+    dfs=[]
+    for indx,file in enumerate(file_paths):
+        # Read file
+        df=pd.read_excel(file,sheet_name=sheet_names[indx])
+        # add success column based on input
+        df['__success_header__'] = np.where(df[success_header].isin(success_input), 'ok', 'fail')
+
+        # Add file to dictionary
+        dfs.append(df)
+
+
+
+    # 2. define target columns based on success_criterion
+    if success_criterion == "time":
+        column_name=time_header
+        x_name='Time [s]'
+        more_is_better=False
+    elif success_criterion== "objective":
+        column_name=best_obj_header
+        x_name='Objective'
+        more_is_better=False
+    elif success_criterion== "bound":
+        column_name=bound_header
+        x_name='Obj. lower bound'
+        more_is_better=True
+    elif success_criterion== "gap":
+        column_name=gap_header
+        x_name='Obj. gap [%]'
+        more_is_better=False
+    elif success_criterion=="objective-distance":
+        column_name=success_criterion
+        x_name='Obj. - Best obj.'
+        more_is_better=False
+    elif success_criterion=="bound-distance":
+        column_name=success_criterion
+        x_name='Best bound - Bound'
+        more_is_better=False
     else:
-        raise ValueError("Invalid success_criterion! Choose from 'time','gap', 'objective', or 'bound'.") 
-    
-    # Dictionary to store performance data for each method
-    performance_data = {}
+        raise ValueError("Invalid success_criterion") 
 
-    # Get all sheet names
-    xls = pd.ExcelFile(file_path)
-    sheet_names = xls.sheet_names
-    model_names=['MIP','MInP(1)','MInLiP(1)','MInP(2)','MInLiP(2)']
+    # Adding new headers for bound distance and objective distance
+    if success_criterion=="bound-distance" or success_criterion=="objective-distance":
 
-    for k in range(len(model_names)):
-        sheet=sheet_names[k]
-        model=model_names[k]
+        # Stack the column from each df into a 2D array
+        if success_criterion=="objective-distance":
+            #  use np.inf for failed rows
+            column_matrix = np.array([
+                np.where(
+                    (df['__success_header__'] == 'fail') | (~df[best_obj_header].apply(is_real_numeric)),
+                    np.inf,
+                    df[best_obj_header].values
+                )
+                for df in dfs
+            ])
 
-        # Load the data
-        df = pd.read_excel(xls, sheet_name=sheet)
-        
-        # Compute the fraction of successfully solved problems
+            row_min = np.min(column_matrix, axis=0)
+
+
+            for df in dfs:
+                df[column_name] = df[best_obj_header]- row_min
+
+        else:
+            #  use np.inf for failed rows
+            column_matrix = np.array([
+                np.where(
+                    (df['__success_header__'] == 'fail') | (~df[bound_header].apply(is_real_numeric)),
+                    -np.inf,
+                    df[bound_header].values
+                )
+                for df in dfs
+            ])
+            
+            row_max = np.max(column_matrix, axis=0)
+            # print(row_max)
+
+            for df in dfs:
+                df[column_name] = row_max - df[bound_header]
+
+
+    #3. Find minimum and maximum
+
+    # Filter for succesful runs
+    filter_success_for_min_max = lambda df: df[(df['__success_header__'] == 'ok') & df[column_name].apply(is_real_numeric)][column_name]
+
+
+    min_candidates = [filter_success_for_min_max(df).min() for df in dfs]
+    max_candidates = [filter_success_for_min_max(df).max() for df in dfs]
+
+    min_value = min(min_candidates, default=0)
+    max_value = max(max_candidates, default=1)
+
+    if not min_candidates:
+        dimensionless_x_axis=True
+
+
+
+    # 4. Define dimensionless success value
+    for df in dfs:
+        df['__dimensionless__']=np.where(df['__success_header__'] == 'ok',(df[column_name]-min_value)/(max_value-min_value),-np.inf if more_is_better else np.inf)
+
+    # 5. Compute the fraction of successfully solved problems and plot
+
+    plt.figure(figsize=(10, 6))  
+    tau_range = np.arange(0, 1, tau)
+    if 1 not in tau_range:
+        tau_range = np.append(tau_range, 1)
+
+    for index,name in enumerate(alg_names):
+        df=dfs[index]
         success_rates = []
+        x_axes_val=[]
         total_problems = len(df)
         
         for it in tau_range:
-            success_count=((df[column_name]<=it)).sum()
+            if more_is_better:
+                condition=df['__dimensionless__']>=it
+            else:
+                condition=df['__dimensionless__']<=it
+            success_count=(condition).sum()
             success_rate = success_count / total_problems
             success_rates.append(success_rate)
+            x_axes_val.append(min_value+it*(max_value-min_value))
         
-        # Store the results
-        performance_data[model] = success_rates
 
         # Plot the performance profile for this method
-        plt.plot(tau_range, success_rates, label=model)
+        if dimensionless_x_axis:
+            plt.plot(tau_range, success_rates, label=name)
+            add='Dimensionless'
+        
+        else:
+            plt.plot(x_axes_val, success_rates, label=name)
+            add=''
 
     # Formatting the plot
-    plt.xlabel("Desired DQ")
-    plt.ylabel("Fraction of Problems successfully solved:\nDQ<=Desired DQ")
-    plt.title("Dimensionless quality (DQ):\n"+x_name)
+    if more_is_better:
+        sign='>='
+    else:
+        sign='<='
+
+    if len(success_input)>1:
+        success_str = ' or '.join(str(s) for s in success_input)
+    else:
+        success_str = str(success_input[0])
+
+
+    plt.xlabel(f"x: {add} {x_name}")
+    plt.ylabel(
+        f"Fraction of problems with: \n"
+        f"{success_header} = {success_str}\n"
+        f"{add} {x_name} {sign} x"
+    )
     plt.legend()
+    # plt.xscale("log")
     plt.grid(True)
     plt.show()
 
 
-
 if __name__ == '__main__':
+
+
+    # ---Fixed configuration---
+    sheet_names=['Formulation_1','Formulation_2','Formulation_3','Formulation_4','Formulation_5']
+    alg_names=['MIP','MInP(1)','MInLiP(1)','MInP(2)','MInLiP(2)']
+    best_obj_headers='Objective'
+    time_header='Time [s]'
+    bound_header='Obj. bound'
+    gap_header='Obj. gap'
+    success_header='Status'
+    success_input=['HxSolutionStatus.OPTIMAL','HxSolutionStatus.FEASIBLE']
+    tau=0.001
+    dimensionless_x_axis=False
+
+    # ---Original formulation---
+
     file_name="original.xlsx"
     file_path = Path("./hexaly_benchmarking_results/"+file_name)
+    file_paths=[str(file_path)]*5
 
-    plot_data_profiles(file_path, success_criterion="time",tau=1e-3,upper=1)
+    # obj distance
+    success_criterion="objective-distance"
+    performance_plots(file_paths,sheet_names,alg_names,best_obj_headers,time_header,bound_header,gap_header,success_header,success_input,success_criterion,tau=tau,dimensionless_x_axis=dimensionless_x_axis)
 
-    plot_data_profiles(file_path, success_criterion="gap",tau=1e-3,upper=1)
+    # bound distance
+    success_criterion="bound-distance"
+    performance_plots(file_paths,sheet_names,alg_names,best_obj_headers,time_header,bound_header,gap_header,success_header,success_input,success_criterion,tau=tau,dimensionless_x_axis=dimensionless_x_axis)
 
-    plot_data_profiles(file_path, success_criterion="objective",tau=1e-3,upper=1)
+    # time
+    success_criterion="time"
+    performance_plots(file_paths,sheet_names,alg_names,best_obj_headers,time_header,bound_header,gap_header,success_header,success_input,success_criterion,tau=tau,dimensionless_x_axis=dimensionless_x_axis)
 
-    plot_data_profiles(file_path, success_criterion="bound",tau=1e-3,upper=1)
+    # gap
+    success_criterion="gap"
+    performance_plots(file_paths,sheet_names,alg_names,best_obj_headers,time_header,bound_header,gap_header,success_header,success_input,success_criterion,tau=tau,dimensionless_x_axis=dimensionless_x_axis)
+
+
+
+
+    # ---Simplified formulation---
 
     file_name="known_n.xlsx"
     file_path = Path("./hexaly_benchmarking_results/"+file_name)
+    file_paths=[str(file_path)]*5
 
 
-    plot_data_profiles(file_path, success_criterion="time",tau=1e-3,upper=1)
+    # obj distance
+    success_criterion="objective-distance"
+    performance_plots(file_paths,sheet_names,alg_names,best_obj_headers,time_header,bound_header,gap_header,success_header,success_input,success_criterion,tau=tau,dimensionless_x_axis=dimensionless_x_axis)
 
-    plot_data_profiles(file_path, success_criterion="gap",tau=1e-3,upper=1)
+    # bound distance
+    success_criterion="bound-distance"
+    performance_plots(file_paths,sheet_names,alg_names,best_obj_headers,time_header,bound_header,gap_header,success_header,success_input,success_criterion,tau=tau,dimensionless_x_axis=dimensionless_x_axis)
 
-    plot_data_profiles(file_path, success_criterion="objective",tau=1e-3,upper=1)
+    # time
+    success_criterion="time"
+    performance_plots(file_paths,sheet_names,alg_names,best_obj_headers,time_header,bound_header,gap_header,success_header,success_input,success_criterion,tau=tau,dimensionless_x_axis=dimensionless_x_axis)
 
-    plot_data_profiles(file_path, success_criterion="objective",tau=1e-8,upper=1e-5)
-
-    plot_data_profiles(file_path, success_criterion="bound",tau=1e-3,upper=1)
+    # gap
+    success_criterion="gap"
+    performance_plots(file_paths,sheet_names,alg_names,best_obj_headers,time_header,bound_header,gap_header,success_header,success_input,success_criterion,tau=tau,dimensionless_x_axis=dimensionless_x_axis)
